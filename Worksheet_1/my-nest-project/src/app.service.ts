93cf56faf6fdc7ad5496e5f580d39f335ec5a295
Samalu45 <samalu45@gmail.com>
2024-07-23 18:21:23 +1200

    Commit for Evidence
    A commit with the purpose of showing steps on how to push in the evicence sheet

--- Worksheet_1/my-nest-project/src/app.service.ts
+++ Worksheet_1/my-nest-project/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Modified for test!';
+    return 'Hello World! Modified for test! Test for the evidence';
   }
 }
